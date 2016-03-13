f988ac72cb141414a6d9e10d5d370c9f73f5e6cc
BuildTools <unconfigured@null.spigotmc.org>
2016-03-13 16:24:55 -0700

    cleaned up and simplified the whole thing. It works now, too

--- ColorHelper.py
+++ ColorHelper.py
@@ -1,40 +1,28 @@
 import colorsys
 
+# main color conversion functions
 def hex6ToRgbInt(color):
     return(int(color[-6:-4], 16), int(color[-4:-2], 16), int(color[-2:], 16))
 
-def hex6ToRgbFloat(color):
-    return(rgbIntToRgbFloat(hex6ToRgbInt(color)))
+def rgbFloatToHslFloat(color):
+    result = colorsys.rgb_to_hls(color[0], color[1], color[2])
+    return(result[0], result[2], result[1])
 
-def hex6ToHslFloat(color):
-    rgbFloatColor = hex6ToRgbFloat(color)
-    return(colorsys.rgb_to_hls(rgbFloatColor[0], rgbFloatColor[1], rgbFloatColor[2]))
-
-def hex6ToHslInt(color):
-    hslFloatColor = hex6ToHslFloat(color)
-    return(int(round(hslFloatColor[0]*360)), int(round(hslFloatColor[2]*100)), int(round(hslFloatColor[1]*100)))
-
-def hslIntToRgbFloat(color):
-    hslFloatColor = hslIntToHslFloat(color)
-    return(colorsys.hls_to_rgb(hslFloatColor[0], hslFloatColor[2], hslFloatColor[1]))
-
-#not working
-def hslIntToRgbInt(color):
-    hslFloatColor = hslIntToHslFloat(color)
-    return(colorsys.hls_to_rgb(hslFloatColor[0], hslFloatColor[2], hslFloatColor[1]))
-
-
-def hlsIntToRgbInt(color):
-    return(rgbFloatToRgbInt(hslIntToRgbFloat))
+def hslFloatToRgbFloat(color):
+    return(colorsys.hls_to_rgb(color[0], color[2], color[1]))
 
 def rgbIntToRgbFloat(color):
     return(color[0]/256, color[1]/256, color[2]/256)
 
 def rgbFloatToRgbInt(color):
-    return(color[0]*256, color[1]*256, color[2]*256)
+    return(int(round(color[0]*256)), int(round(color[1]*256)), int(round(color[2]*256)))
 
 def hslFloatToHslInt(color):
-    return(color[0]/256, color[1]/256, color[2]/256)
+    return(int(round(color[0]*360)), int(round(color[1]*100)), int(round(color[2]*100)))
 
 def hslIntToHslFloat(color):
-    return(color[0]*256, color[1]*256, color[2]*256)
+    return(color[0]/256, color[1]/256, color[2]/256)
+
+# helper color conversion methods
+def hslIntComplement(color):
+    return((color[0]+180)%360, color[1], color[2])
