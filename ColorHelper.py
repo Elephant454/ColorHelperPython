b131661cbaa3aaa66e641218b0083d4060489c53
Matthew Sullivan <elephant454@gmail.com>
2016-03-30 14:24:22 -0700

    Simplified everything and everything works. This is the new stable basis for the library.

--- ColorHelper.py
+++ ColorHelper.py
@@ -1,35 +1,13 @@
-import colorsys
-
 # main color conversion functions
-def hex6ToRgbInt(color):
+def hexToRgb(color):
     return(int(color[-6:-4], 16), int(color[-4:-2], 16), int(color[-2:], 16))
 
-#def rgbFloatToHslFloat(color):
-#    result = colorsys.rgb_to_hls(color[0], color[1], color[2])
-#    return(result[0], result[2], result[1])
-
-#def hslFloatToRgbFloat(color):
-#    return(colorsys.hls_to_rgb(color[0], color[2], color[1]))
-
-def rgbIntToRgbFloat(color):
-    return(color[0]/255, color[1]/255, color[2]/255)
-
-def rgbFloatToRgbInt(color):
-    return(int(round(color[0]*255)), int(round(color[1]*255)), int(round(color[2]*255)))
-
-def hslFloatToHslInt(color):
-    return(int(round(color[0]*359)), int(round(color[1]*99)), int(round(color[2]*99)))
-
-def hslIntToHslFloat(color):
-    return(color[0]/359, color[1]/99, color[2]/99)
-
-# helper color conversion methods
-def hslIntComplement(color):
-    return((color[0]+180)%359, color[1], color[2])
-
-def rgbFloatToHslFloat(color):
-    rgbMin = min(color[0], color[1], color[2])
-    rgbMax = max(color[0], color[1], color[2])
+def rgbToHsl(color):
+    r = color[0]/255
+    g = color[1]/255
+    b = color[2]/255
+    rgbMin = min(r, g, b)
+    rgbMax = max(r, g, b)
     deltaRgb = rgbMax - rgbMin
 
     l = (rgbMin+rgbMax) / 2
@@ -42,15 +20,15 @@ def rgbFloatToHslFloat(color):
         else:
             s = deltaRgb / (2 - rgbMax - rgbMin)
 
-        deltaR = (((rgbMax-color[0]) / 6) + (rgbMax/2)) / deltaRgb
-        deltaG = (((rgbMax-color[1]) / 6) + (rgbMax/2)) / deltaRgb
-        deltaB = (((rgbMax-color[2]) / 6) + (rgbMax/2)) / deltaRgb
+        deltaR = (((rgbMax-r) / 6) + (rgbMax/2)) / deltaRgb
+        deltaG = (((rgbMax-g) / 6) + (rgbMax/2)) / deltaRgb
+        deltaB = (((rgbMax-b) / 6) + (rgbMax/2)) / deltaRgb
 
-        if(color[0] == rgbMax):
+        if(r == rgbMax):
             h = deltaB - deltaG
-        elif(color[1] == rgbMax):
+        elif(g == rgbMax):
             h = (1/3) + deltaR - deltaB
-        elif(color[2] == rgbMax):
+        elif(b == rgbMax):
             h = (2/3) + deltaG - deltaR
 
         if(h<0):
@@ -58,9 +36,9 @@ def rgbFloatToHslFloat(color):
         if(h>1):
             h -= 1
 
-    return(h, s, l)
+    return(int(round(h*360)), int(round(s*100)), int(round(l*100)))
 
-def hslFloatToRgbFloat(color):
+def hslToRgb(color):
     def hueToRgb(v1, v2, vH):
         if(vH < 0):
             vH += 1
@@ -74,20 +52,28 @@ def hslFloatToRgbFloat(color):
             return(v1 + (v2-v1) * ((2/3) - vH) * 6)
         return(v1)
 
-    if(color[1]==0):
-        r = color[2]*255
-        g = color[2]*255
-        b = color[2]*255
+    h = color[0] / 360
+    s = color[1] / 100
+    l = color[2] / 100
+
+    if(l==0):
+        r = l*255
+        g = l*255
+        b = l*255
     else:
-        if(color[2]<0.5):
-            v2 = color[2] * (1+color[1])
+        if(l<0.5):
+            v2 = l * (1+s)
         else:
-            v2 = (color[2] + color[1]) - (color[1] * color[2])
+            v2 = (l + s) - (s * l)
 
-        v1 = 2 * color[2] - v2
+        v1 = 2 * l - v2
 
-        r = 255 * hueToRgb(v1, v2, color[0] + (1/3))
-        g = 255 * hueToRgb(v1, v2, color[0])
-        b = 255 * hueToRgb(v1, v2, color[0] - (1/3))
+        r = 255 * hueToRgb(v1, v2, h + (1/3))
+        g = 255 * hueToRgb(v1, v2, h)
+        b = 255 * hueToRgb(v1, v2, h - (1/3))
 
-    return(r, g, b)
+    return(int(round(r)), int(round(g)),int(round(b)))
+
+# helper color conversion methods
+def hslIntComplement(color):
+    return((color[0]+180)%360, color[1], color[2])
